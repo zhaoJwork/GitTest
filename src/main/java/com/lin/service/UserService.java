d64dfe6ecf5cfa984ce4a0245e515fb486b28180
liweizhi <j.lyang@163.com>
2018-06-13 19:01:14 +0800

    '修改个人详情查询,职务、辅助表实体生成'

--- src/main/java/com/lin/service/UserService.java
+++ src/main/java/com/lin/service/UserService.java
@@ -1,13 +1,15 @@
 package com.lin.service;
 
 import com.ideal.wheel.common.AbstractService;
-import com.lin.domain.QUser;
-import com.lin.domain.User;
+import com.lin.domain.*;
 import com.lin.repository.UserRepository;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * 功能概要：UserService接口类
@@ -33,5 +35,24 @@ public class UserService extends AbstractService<User,String> {
 		JPAQueryFactory query = jpaQueryFactory();
 
 		return query.select(user).from(user).where(user.userID.in(strings)).fetch();
+	}
+
+	/**
+	 * 根据人员ID查询个人详情
+	 * @param userID
+	 * @return
+	 */
+	public UserDetailsDsl selectUserDetails(String userID){
+		QUser user = QUser.user;
+		QUserNewAssistDsl uass = QUserNewAssistDsl.userNewAssistDsl;
+		return jpaQueryFactory().select(Projections.bean(UserDetailsDsl.class,
+				user.userID,
+				user.userName,
+				new CaseBuilder().when(uass.portrait_url.eq("").or(uass.portrait_url.isNull())).then(user.userPic).otherwise(uass.portrait_url).as("userPic")
+
+		)).from(user)
+				.leftJoin(uass)
+				.on(user.userID.eq(uass.userid))
+				.where(user.userID.eq(userID)).fetchOne();
 	}
 }
