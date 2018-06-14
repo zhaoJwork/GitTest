5cfbaa8731cdac45a2eb5e147033285dd0e5e75d
刘东东 <18201407550@163.com>
2018-06-14 15:53:07 +0800

    返回类型修改string类型

---
+++ src/main/java/com/lin/domain/AutoCollection.java
@@ -0,0 +1,289 @@
+package com.lin.domain;
+
+public class AutoCollection {
+
+	// address_collection
+	private Integer rowId;
+	
+	private Integer source;
+	
+	// address_user
+	private String userID;
+	
+	private String userName;
+	
+	private String provinceId;
+	
+	private String phone;
+	
+	private String emaile;
+	
+	private String address;
+	
+	private String organizationId;
+	
+	private String userPic;
+	
+	private String uupdateDate;
+	
+	private String type;
+	
+	private String context;
+	
+	private String field;
+	
+	private String posName;
+	
+	private String deptype;
+	
+	private String quanpin;
+	
+	private String shouZiMu;
+	
+	private String hytAccount;
+	
+	private String crmAccount;
+	
+	private String flagOnline;
+	
+	private Integer sortNum;
+	
+	private Integer orderNum;
+	
+	// address_user_new
+	private Integer install;
+
+
+	
+	
+	public Integer getRowId() {
+		return rowId;
+	}
+
+	public void setRowId(Integer rowId) {
+		this.rowId = rowId;
+	}
+
+	public Integer getSource() {
+		return source;
+	}
+
+	public void setSource(Integer source) {
+		this.source = source;
+	}
+
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getProvinceId() {
+		return provinceId;
+	}
+
+	public void setProvinceId(String provinceId) {
+		this.provinceId = provinceId;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmaile() {
+		return emaile;
+	}
+
+	public void setEmaile(String emaile) {
+		this.emaile = emaile;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getOrganizationId() {
+		return organizationId;
+	}
+
+	public void setOrganizationId(String organizationId) {
+		this.organizationId = organizationId;
+	}
+
+	public String getUserPic() {
+		return userPic;
+	}
+
+	public void setUserPic(String userPic) {
+		this.userPic = userPic;
+	}
+
+	public String getUupdateDate() {
+		return uupdateDate;
+	}
+
+	public void setUupdateDate(String uupdateDate) {
+		this.uupdateDate = uupdateDate;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getContext() {
+		return context;
+	}
+	
+	
+
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
+	public void setContext(String context) {
+		this.context = context;
+	}
+
+
+
+	public String getPosName() {
+		return posName;
+	}
+
+	public void setPosName(String posName) {
+		this.posName = posName;
+	}
+
+	public String getDeptype() {
+		return deptype;
+	}
+
+	public void setDeptype(String deptype) {
+		this.deptype = deptype;
+	}
+
+	public String getQuanpin() {
+		return quanpin;
+	}
+
+	public void setQuanpin(String quanpin) {
+		this.quanpin = quanpin;
+	}
+
+	public String getShouZiMu() {
+		return shouZiMu;
+	}
+
+	public void setShouZiMu(String shouZiMu) {
+		this.shouZiMu = shouZiMu;
+	}
+
+	public String getHytAccount() {
+		return hytAccount;
+	}
+
+	public void setHytAccount(String hytAccount) {
+		this.hytAccount = hytAccount;
+	}
+
+	public String getCrmAccount() {
+		return crmAccount;
+	}
+
+	public void setCrmAccount(String crmAccount) {
+		this.crmAccount = crmAccount;
+	}
+
+	public String getFlagOnline() {
+		return flagOnline;
+	}
+
+	public void setFlagOnline(String flagOnline) {
+		this.flagOnline = flagOnline;
+	}
+
+	public Integer getSortNum() {
+		return sortNum;
+	}
+
+	public void setSortNum(Integer sortNum) {
+		this.sortNum = sortNum;
+	}
+
+	public Integer getOrderNum() {
+		return orderNum;
+	}
+
+	public void setOrderNum(Integer orderNum) {
+		this.orderNum = orderNum;
+	}
+
+	public Integer getInstall() {
+		return install;
+	}
+
+	public void setInstall(Integer install) {
+		this.install = install;
+	}
+
+	@Override
+	public String toString() {
+		return "AutoCollection [rowId=" + rowId + ", source=" + source + ", userID=" + userID + ", userName=" + userName
+				+ ", provinceId=" + provinceId + ", phone=" + phone + ", emaile=" + emaile + ", address=" + address
+				+ ", organizationId=" + organizationId + ", userPic=" + userPic + ", uupdateDate=" + uupdateDate
+				+ ", type=" + type + ", context=" + context + ", field=" + field + ", posName=" + posName + ", deptype="
+				+ deptype + ", quanpin=" + quanpin + ", shouZiMu=" + shouZiMu + ", hytAccount=" + hytAccount
+				+ ", crmAccount=" + crmAccount + ", flagOnline=" + flagOnline + ", sortNum=" + sortNum + ", orderNum="
+				+ orderNum + ", install=" + install + "]";
+	}
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
