35c7c6652c4a9256063839a2c601c4b3c98ea541
liweizhi <j.lyang@163.com>
2018-06-15 11:59:25 +0800

    合并代码

--- src/main/java/com/lin/service/UserService.java
+++ src/main/java/com/lin/service/UserService.java
@@ -9,6 +9,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
